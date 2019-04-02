46afadf040e08b8b965467e99c7d718ee18039d1
ksnam <david.nam@pulmuone.com>
2019-04-02 20:29:33 +0900

    LESSON 3

---
+++ src/main/resources/data-h2-stage.sql
@@ -0,0 +1,2 @@
+insert into posts (ID, title, author, content, created_date, modified_date) values (99998,'스테이지 테스트1', 'test1@gmail.com', '테스트1의 본문', now(), now());
+insert into posts (ID, title, author, content, created_date, modified_date) values (99999,'스테이지 테스트2', 'test2@gmail.com', '테스트2의 본문', now(), now());
